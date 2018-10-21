982cdf3d78db2d3f2c8b73139c6d6043d78af703
liu <806020149@qq.com>
2018-10-21 13:54:38 +0800

    img

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,35 +1,35 @@
 <template>
     <div class="user">
         <div class="user-login">
-            <rout-link to="javascript:;">
+            <router-link to="/user/login">
                 <div class="user-login-head">
                     <div><img src="http://localhost:8085/img/users.png" alt=""></div>
                     <div>登录/注册</div>
                 </div>
-            </rout-link>
+            </router-link>
             <div class="user-login-two">
                 <div>我的订单</div>
                 <div>全部订单</div>
             </div>
             <div class="user-login-three">
                 <a href="">
-                    <div><img src="" alt="123"><p>代付款</p></div>
+                    <div><img src="http://localhost:8085/img/fukuan.png" alt="123"><p>代付款</p></div>
                 </a>
                 <a href="">
-                    <div><img src="" alt="123"><p>待收货</p></div>
+                    <div><img src="http://localhost:8085/img/shouhuo.png" alt="123"><p>待收货</p></div>
                 </a>
                 <a href="">
-                    <div><img src="" alt="123"><p>退返修</p></div>
+                    <div><img src="http://localhost:8085/img/banshou.png" alt="123"><p>退返修</p></div>
                 </a>
             </div>
         </div>
         <div class="user-login-four">
-            <div class="user-login-four-item"><img src="" alt="123">会员中心</div>
-            <div class="user-login-four-item"><img src="" alt="123">我的优惠</div>
+            <div class="user-login-four-item"><span><img class="icon" src="http://localhost:8085/img/huiyuan.png" alt="123">会员中心</span><img class="back" src="http://localhost:8085/img/back.png" alt=""></div>
+            <div class="user-login-four-item"><span><img class="icon" src="http://localhost:8085/img/me.png" alt="123">我的优惠</span><img class="back" src="http://localhost:8085/img/back.png" alt=""></div>
         </div>
         <div class="user-item">
-            <div class="user-login-five-item">服务中心</div>
-            <div class="user-login-five-item">小米之家</div>
+            <div class="user-login-five-item"><span><img class="icon" src="http://localhost:8085/img/serve.png" alt="123">服务中心</span><img class="back" src="http://localhost:8085/img/back.png" alt=""></div>
+            <div class="user-login-five-item"><span><img class="icon" src="http://localhost:8085/img/home.png" alt="123">小米之家</span><img class="back" src="http://localhost:8085/img/back.png" alt=""></div>
         </div>
     </div>
 </template>
@@ -43,6 +43,7 @@ export default {
 </script>
 
 <style scoped>
+
 .user-login .user-login-head{
     display: flex;
     background: #f37d0f url("http://localhost:8085/img/bg.png") center 0;
@@ -75,11 +76,30 @@ export default {
 .user-login-three{
     justify-content: space-around;
 }
+.user-login-three div{
+    text-align: center;
+}
 .user-login-three div img{
     width: 30px;
     height: 30px;
 }
 .user-login-four-item, .user-login-five-item{
+    display: flex;
+    justify-content: space-between;
     padding: 15px 10px;
+    border-bottom: 1px solid #D9D9D9;
+    line-height: 30px;
+    box-sizing: border-box;
+}
+.back{
+    transform: rotateY(180deg) scale(.5);
+    vertical-align: middle;
+}
+.icon{
+    width: 30px;
+    height: 30px;
+    vertical-align: middle;
+    margin-right: 15px;
+    /* margin-bottom: 10px; */
 }
 </style>
